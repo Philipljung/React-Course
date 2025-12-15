e7f30b96798ed65911868e583755782e1573ab74
Philipljung <philipljung04@gmail.com>
2025-12-15 21:07:19 +0100

    Fixed searchbar

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,16 +1,29 @@
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
 import "./header.css";
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogo from '../assets/images/mobile-logo.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import CartIcon from '../assets/images/icons/cart-icon.png';
+import { useState } from 'react';
 
 export function Header({ cart = [] }) {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
     let totalQuantity = 0;
 
     cart.forEach(( cartItem ) => {
         totalQuantity += cartItem.quantity;
     });
+
+    const searchInput = (event) => {
+        setSearch(event.target.value);
+        console.log(search);
+    };
+
+    const searchBtn = () => {
+        navigate(`/?search=${search}`)
+    }
 
     return (
         <div className="header">
@@ -24,9 +37,13 @@ export function Header({ cart = [] }) {
             </div>
 
             <div className="middle-section">
-                <input className="search-bar" type="text" placeholder="Search" />
+                <input className="search-bar" type="text" placeholder="Search" 
+                onChange={searchInput}
+                />
 
-                <button className="search-button">
+                <button className="search-button"
+                onClick={searchBtn}
+                >
                     <img className="search-icon" src={SearchIcon} />
                 </button>
             </div>
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -3,33 +3,35 @@ import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import "./HomePage.css";
+import { useSearchParams } from 'react-router';
 
 export function HomePage({ cart, loadCart }) {
     const [products, setProducts] = useState([]);
-    
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
 
     useEffect(() => {
         const getHomeData = async () => {
-            const response = await axios.get('/api/products');
+            const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+            const response = await axios.get(urlPath);
             setProducts(response.data)
-        };                                                      //7:53:00
-        
+        };                                                                                  //7:53:00
+
         getHomeData();
-    }, []);
-    
+    }, [search]);
 
     return (
         <>
             <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
             <title>Ecommerce Project</title>
-            <Header 
-            cart={cart}
+            <Header
+                cart={cart}
             />
 
             <div className="home-page">
-                <ProductsGrid 
-                products={products}
-                loadCart={loadCart}
+                <ProductsGrid
+                    products={products}
+                    loadCart={loadCart}
                 />
             </div>
         </>
